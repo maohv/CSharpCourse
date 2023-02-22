770f07c4c0dc231b2d892de1d47762f8941e68eb
{Zin} <{maohvhn@gmail.com}>
2023-02-22 16:31:16 +0700

    update 67 68 69

---
+++ CSharpCourse/Lesson67.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourse
+{
+    class Lesson67
+    {
+        //Tìm hiểu và sử dụng lớp Stack<T>
+        static void Main()
+        {
+            Console.OutputEncoding= Encoding.UTF8;
+            Stack<string> stack1 = new Stack<string>();
+            Stack<string> stack2 = new Stack<string>(new string[] {"one", "two", "three"});
+            Console.WriteLine($"=> Số phần tử trong stack: {stack2.Count}");
+            stack2.Push("four"); //thêm phần tử vào stack
+            //stack2.Clear(); //xóa toàn bộ phần tử
+            //stack2.Contains("one"); //tìm kiếm phần tử trong stack có hay ko
+            Console.WriteLine($"=> Phần tử top: {stack2.Peek()}");
+            stack2.Pop();//xóa bỏ 1 phần tử
+            Console.WriteLine($"=> Phần tử top: {stack2.Peek()}");
+
+            Console.WriteLine("=> các phần tử có trong stack");
+            var elements = stack2.ToArray();
+            foreach (var item in elements)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Phần tử \"three\" có tồn tại trong stack? " + stack2.Contains("three"));
+        }
+    }
+}
---
+++ CSharpCourse/Lesson69.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpCourse
+{
+    class Lesson69
+    {
+        // Lớp LinkedList<T> danh sách liên kết đôi có sẵn của thư viện
+        static void Main()
+        {
+            Console.OutputEncoding= Encoding.UTF8;
+            var elements = new string[] {"Monday", "Tuesday", "Wednesday", "Thursday"};
+            LinkedList<string> list = new LinkedList<string>(elements);
+            Console.WriteLine($"Số phần tử của DSLK: {list.Count}");
+            Console.WriteLine($"Số phần tử đầu của DSLK: {list.First.Value}");
+            Console.WriteLine($"Số phần tử cuối của DSLK: {list.Last.Value}");
+            Console.WriteLine($"Số phần tử tiếp theo của của phần tử First của DSLK: {list.First.Next.Value}");
+            Console.WriteLine($"Phần tử liền trước của phần tử First của DSLK: {list.First.Previous}");
+        }
+    }
+}
