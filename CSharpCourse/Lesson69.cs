c2d5bb675cd0a395a73dc320fc458fe0d21fffea
{Zin} <{maohvhn@gmail.com}>
2023-02-23 16:44:25 +0700

    update 69 70

--- CSharpCourse/Lesson69.cs
+++ CSharpCourse/Lesson69.cs
@@ -18,7 +18,46 @@ namespace CSharpCourse
             Console.WriteLine($"Số phần tử đầu của DSLK: {list.First.Value}");
             Console.WriteLine($"Số phần tử cuối của DSLK: {list.Last.Value}");
             Console.WriteLine($"Số phần tử tiếp theo của của phần tử First của DSLK: {list.First.Next.Value}");
-            Console.WriteLine($"Phần tử liền trước của phần tử First của DSLK: {list.First.Previous}");
+            Console.WriteLine($"Phần tử liền trước của phần tử First của DSLK: {(list.First.Previous == null ? "null" : "")}");
+
+            //Lấy node thứ 2 của danh sách
+            LinkedListNode<string> node = node = list.First.Next;
+            //các phương thức
+            list.AddFirst("Sunday"); //thêm đầu của danh sách
+            list.AddLast("Friday"); //thêm vào cuối ds
+            Console.WriteLine("=> các phần tử trong danh sách hiện tại: ");
+            ShowNodes(list);
+
+            list.AddAfter(node, "Saturday");
+            list.AddBefore(node, "Saturday");
+            Console.WriteLine("=> Các phần tử danh sách liên kết sau khi chèn: ");
+            ShowNodes(list);
+
+            //xóa node đầu
+            list.RemoveFirst();
+            Console.WriteLine("=> Các phần tử danh sách liên kết sau khi xóa node đầu: ");
+            ShowNodes(list);
+            //xóa node cuối
+            list.RemoveLast();
+            Console.WriteLine("=> Các phần tử danh sách liên kết sau khi xóa node cuối: ");
+            ShowNodes(list);
+            //xóa node được chỉ định
+            list.Remove("Wednesday");
+            Console.WriteLine("=> Các phần tử danh sách liên kết sau khi xóa phần tử được chỉ định: ");
+            ShowNodes(list);
+
+            //kiểm tra một giá trị tồn tại hay không?
+            Console.WriteLine("=> 'Friday' có tồn tại trong DSLK? " + list.Contains("Friday"));
+            Console.WriteLine("=> 'Monday' có tồn tại trong DSLK? " + list.Contains("Monday"));
+        }
+        private static void ShowNodes(LinkedList<string> list)
+        {
+            
+            foreach (var item in list)
+            {
+                Console.Write(item + " -> ");
+            }
+            Console.WriteLine("null");
         }
     }
 }
