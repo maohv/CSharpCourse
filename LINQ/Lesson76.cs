a3cbcf07fc0d81e71a1d7590eddad3fc1720685a
PC <maohvhn@gmail.com>
2023-02-27 22:16:40 +0700

    update LINQ

---
+++ LINQ/Lesson76.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Lesson76
+    {
+        //Mối quan hệ về kiểu dữ liệu trong LINQ
+        static void Main()
+        {
+
+        }
+    }
+}
---
+++ LINQ/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    public class Student
+    {
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public Student(string id, string fullName, string address)
+        {
+            Id = id;
+            FullName = fullName;
+            Address = address;
+        }
+        public override string ToString() => $"Student[{Id}, {FullName}, {Address}]";
+    }
+}
