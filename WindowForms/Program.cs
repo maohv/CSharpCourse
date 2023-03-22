65493d282fefce6a91ed0b67e58bfad154d3f326
{Zin} <{maohvhn@gmail.com}>
2023-03-22 16:52:01 +0700

    update learnLableTextbox winform

--- WindowForms/Program.cs
+++ WindowForms/Program.cs
@@ -17,7 +17,7 @@ namespace WindowForms
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new Form1());
-            Application.Run(new LearnButtonForm());
+            Application.Run(new LearnLabelTextbox());
         }
     }
 }
